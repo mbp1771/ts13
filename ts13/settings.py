3362a67ae28eb75738aeb28ea0668bcba93f61e6
mbp1771 <bhanum1771@gmail.com>
2022-09-18 15:55:00 +0530

    settings

--- ts13/settings.py
+++ ts13/settings.py
@@ -25,7 +25,7 @@ if 'WEBSITE_HOSTNAME' in os.environ:
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-BASE_URL = 'https://localhost/'
+BASE_URL = 'http://localhost/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -163,9 +163,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 UPLOADED_FILES_USE_URL = True
-
-SESSION_COOKIE_SECURE = False
-
-CSRF_COOKIE_SECURE = False
-
-SECURE_SSL_REDIRECT = False
