d831c8f89487c4e6f818cb6619a9fcddc6b0a358
mbp1771 <bhanum1771@gmail.com>
2022-09-24 21:21:33 +0530

    diff reqs

--- ts13/storage_backends.py
+++ ts13/storage_backends.py
@@ -1,9 +1,9 @@
 from storages.backends.azure_storage import AzureStorage
-from azure.identity import DefaultAzureCredential
+from msrestazure.azure_active_directory import MSIAuthentication
 
 class MediaStorage(AzureStorage):
     account_name = 'musicts13'
     azure_container = 'ts13'
-    token_credential = DefaultAzureCredential()
+    token_credential = MSIAuthentication()
     location = 'media'
     overwrite_files = False
